e63d6588ee3e33e2218ff3e013265c04cbd4995b
florent19961 <florentmely@gmail.com>
2025-05-23 10:40:26 +0200

    functionnal code

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,12 +1,26 @@
-from flask import render_template
+from flask import render_template, request, session, redirect, url_for
 from app.routes import bp
 
 @bp.route('/')
 def index():
     return render_template('index.html')
 
-@bp.route('/configurer_partie')
+@bp.route('/configurer_partie', methods=['GET', 'POST'])
 def configurer_partie():
+    if request.method == 'POST':
+        nb_equipes = int(request.form.get('nb-equipes'))
+        nb_mots = int(request.form.get('nb-mots'))
+        choix_mots = request.form.get('choix-mots')
+        
+        # Ici vous pouvez utiliser ces valeurs pour configurer la partie
+        # Par exemple, les stocker en session :
+        session['nb_equipes'] = nb_equipes
+        session['nb_mots'] = nb_mots
+        session['choix_mots'] = choix_mots
+        
+        # Rediriger vers la page de jeu
+        return redirect(url_for('play'))
+    
     return render_template('configurer_partie.html')
 
 @bp.route('/rules')
